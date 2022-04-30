f1f90638caf9d4bba8317bd54acbedad2dd3d4bc
gz <gas@cock.li>
2022-04-30 10:58:18 +0300

    Refactored, added comments and updated path generation

--- main.go
+++ main.go
@@ -11,13 +11,15 @@ import (
 
 const CreatePath string = "/create"
 
+var PreviousPath int64 = 1
+
 var PATHS = make(map[string]string)
 
 func main() {
 	Init()
 	fmt.Println("Initializing /create handler")
-	http.ListenAndServe("127.0.0.1:8082", nil)
 	fmt.Println("Listening on 127.0.0.1:8082")
+	http.ListenAndServe("127.0.0.1:8082", nil)
 }
 
 // Initialize the basic handlers
@@ -25,6 +27,7 @@ func Init() {
 	http.HandleFunc(CreatePath, createHandler)
 }
 
+// createHandler which vould create new handlers for shortned url
 func createHandler(resp http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
 		fmt.Fprintf(resp,
@@ -40,6 +43,7 @@ func createHandler(resp http.ResponseWriter, req *http.Request) {
 	}
 }
 
+// The function which redirects the request to the url
 func Redirector(resp http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodGet {
 		fmt.Fprintf(resp,
@@ -50,7 +54,12 @@ func Redirector(resp http.ResponseWriter, req *http.Request) {
 	http.Redirect(resp, req, PATHS[req.URL.Path], 301)
 }
 
+// Genereate numeric path based on now unix time and previous path
 func genPath() string {
-	rand.Seed(time.Now().Unix())
-	return string(strconv.Itoa(int(rand.Uint32())))
+	seed := time.Now().Unix() * PreviousPath
+	rand.Seed(seed)
+	ipath := int(rand.Uint32())
+	PreviousPath = int64(ipath)
+	path := strconv.Itoa(ipath)
+	return string(path)
 }
